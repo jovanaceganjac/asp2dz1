44e952044c0c53d9e43fa4d7216f073db339d0cd
jovanaceganjac <jceganjac7@gmail.com>
2025-05-27 11:41:49 +0200

    Create Adresna_funkcija.h

---
+++ asp2/asp2dom3/Adresna_funkcija.h
@@ -0,0 +1,9 @@
+#ifndef CJ220423_ADRESNA_FUNKCIJA_H
+#define CJ220423_ADRESNA_FUNKCIJA_H
+
+class Adresna_funkcija {
+public:
+    virtual int dohvatiadr(int k, int adresa, int brpokusaja) const=0;
+};
+
+#endif
